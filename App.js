6f53a6291499de6bd64998781a3cbde88ed9b41f
TBhS2912 <brunohenriquesanches.29122004@gmail.com>
2023-08-28 08:10:19 -0300

    branch com o código da aula do dia Mon, Aug 28, 2023  8:10:19 AM

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import TelaLogin from "./SRC/TelaLogin"
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    <TelaLogin></TelaLogin>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
